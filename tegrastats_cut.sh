eca763c3fb206e110d3b904628cfa9583f03439b
sachinkmohan <sachinkm308@gmail.com>
2022-06-14 20:05:25 +0200

    jetson data

--- tegrastats_cut.sh
+++ tegrastats_cut.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-input="/home/mohan/todelete/tegrastats.txt"
+input=$1
 while IFS= read -r line
 do
   #awk '/RAM/ {print $2,$10,$14}' >> tegra_out.txt
-  awk '/RAM/ {print $2,$10,$14}' | cut -c 1-4
+  #awk '/RAM/ {print $2,$10,$14}' | cut -c 1-4
+  # sed 's/.$//' removes the last character here, i.e %
+  awk '/RAM/ {print $14}' | sed 's/.$//'
 done < "$input"
